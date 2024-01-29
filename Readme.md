1f26c25c786cab4d1e89dc859c62a546bf7ab6be
ashwani1198 <ashwani.singh@anrxsolutions.com>
2024-01-29 17:49:54 +0530

    db init

--- Readme.md
+++ Readme.md
@@ -1,4 +1,4 @@
-##TODO APP
+## TODO APP
 
 This project contains a simple todo application 
 It has following features :
--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,63 @@
 const express = require('express')
+const { createTodo,updateTodo } =  require("./types")
+const { todo } = require('./db')
 const app = express()
+
 
 const port = 3000
 app.use(express.json())
 
-app.post('/todo',function(req, res){
+app.post('/todo',async function(req, res){
+    const createPayload = req.body
+    const parsedPayload = createTodo.safeParse(createPayload)
+
+    if(!parsedPayload){
+        res.status(411).json({
+            msg : "you sent the wrong inputs"
+        })
+    }
+    await todo.create({
+        title : createPayload.title,
+        description:createPayload.description,
+        completed:false
+    })
+
+    res.json({
+        msg:"todo"
+    })
+
 
 })
 
-app.get('/todos',function(req, res){
-    
+app.get('/todos', async function(req, res){
+    const todos = await todo.find({})
+
+    res.json({
+        todos
+    })
 })
 
-app.put('/completed',function(req, res){
-    
+app.put('/completed',async function(req, res){
+    const updatePayload = req.body
+    const parsedPayload = updateTodo.safeParse(updatePayload)
+
+    if(!parsedPayload){
+        res.status(411).json({
+            msg : "you sent the wrong inputs"
+        })
+    }
+
+    await todo.update({
+        _id : req.body.id
+    },{
+        completed : true
+    })
+    res.json({
+        msg :  `todo - ${req.body.id} marked as completed successfully`
+    })
+
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Todo app listening on port ${port}`)
 })
